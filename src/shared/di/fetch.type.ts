ddfbc757d66884a38a917179a44efa5611296cce
devgiljong <calf-love@naver.com>
2024-04-18 14:26:01 +0900

    :construction: Tanstack store dependency add

--- src/shared/di/fetch.type.ts
+++ src/shared/di/fetch.type.ts
@@ -2,3 +2,6 @@ export type Fetch = (
   input: RequestInfo | URL,
   init?: RequestInit | undefined,
 ) => Promise<Response>;
+
+export type FetchArg = [RequestInfo | URL, RequestInit | undefined];
+export type FetchReturn = Promise<Response>;
