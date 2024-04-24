e8fffcaa9f96bf8e1a0dd85800718b7a3c5e9ba6
devgiljong <calf-love@naver.com>
2024-04-24 15:51:50 +0900

    :construction: Portal example

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,36 @@
 "use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useExampleContext } from "~/src/entities/example";
 import { fetcher } from "~/src/shared/di/api-service.class";
+import Portal from "~/src/shared/ui/portal";
 
 export default function Home() {
   const service = useExampleContext();
   const hi = fetcher(fetch);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
   return (
     <main>
-      dsa
+      <button
+        onClick={async () => {
+          setIsLoading(true);
+
+          await new Promise((res) => setTimeout(res, 200));
+          router.push("/new-page");
+          await new Promise((res) => setTimeout(res, 200));
+
+          setIsLoading(false);
+        }}
+      >
+        누르면 로딩
+      </button>
+      <Portal>
+        {isLoading ? (
+          <div className=" fixed top-0 left-0 w-screen h-screen bg-neutral-200 backdrop-blur-sm z-10"></div>
+        ) : null}
+      </Portal>
       <button
         onClick={async () => {
           const result = await service.getExample();
