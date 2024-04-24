447ba2a94bc842020b7406a9435533b80a21eb75
devgiljong <calf-love@naver.com>
2024-04-24 19:45:08 +0900

    :construction: Test

--- src/shared/ui/portal.tsx
+++ src/shared/ui/portal.tsx
@@ -8,3 +8,5 @@ export default function Portal({ children }: React.PropsWithChildren) {
   React.useEffect(() => setMounted(true), []);
   return mounted ? createPortal(<>{children}</>, document.body) : null;
 }
+
+//test
