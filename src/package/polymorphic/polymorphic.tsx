7e79c2cf64f853dda827daed9435ce1290d3aeba
devgiljong <calf-love@naver.com>
2024-04-28 01:05:58 +0900

    :construction: Polymorphic

---
+++ src/package/polymorphic/polymorphic.tsx
@@ -0,0 +1,28 @@
+import { ElementType, ReactNode, forwardRef } from "react";
+import { PolymorphicComponentProps, PolymorphicComponentPropsWithRef, PolymorphicRef } from "./polymorphic.type";
+
+type Size = "small" | "large";
+
+type Props<C extends ElementType> = PolymorphicComponentProps<
+  C,
+  {
+    size?: Size;
+  }
+>;
+
+type ButtonType = <C extends ElementType = "button">(
+  props: PolymorphicComponentPropsWithRef<C, Props<C>>,
+) => ReactNode | null;
+
+export const Button: ButtonType = forwardRef(function Button<C extends ElementType = "button">(
+  { children, size = "small", as, ...rest }: Props<C>,
+  ref?: PolymorphicRef<C>,
+) {
+  const Component = as || "button";
+
+  return (
+    <Component ref={ref} size={size} {...rest}>
+      {children}
+    </Component>
+  );
+});
