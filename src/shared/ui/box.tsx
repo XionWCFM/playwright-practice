e02d4d10487580c2d41d403f49f951e3d1bf40a9
devgiljong <calf-love@naver.com>
2024-05-05 18:09:19 +0900

    :construction: Box layout 생성

---
+++ src/shared/ui/box.tsx
@@ -0,0 +1,30 @@
+import { ElementType, ReactNode, forwardRef } from "react";
+import {
+  PolymorphicComponentProps,
+  PolymorphicComponentPropsWithRef,
+  PolymorphicRef,
+} from "~/src/package/polymorphic/polymorphic.type";
+
+type Props<C extends ElementType> = PolymorphicComponentProps<
+  C,
+  {
+    className?: string;
+  }
+>;
+
+type BoxType = <C extends ElementType = "div">(
+  props: PolymorphicComponentPropsWithRef<C, Props<C>>,
+) => ReactNode | null;
+
+export const Box: BoxType = forwardRef(function Box<C extends ElementType = "div">(
+  { children, as, className, ...rest }: Props<C>,
+  ref?: PolymorphicRef<C>,
+) {
+  const Component = as || "div";
+
+  return (
+    <Component ref={ref} className={className} {...rest}>
+      {children}
+    </Component>
+  );
+});
