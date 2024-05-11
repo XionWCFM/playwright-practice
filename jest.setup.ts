18a76d037993fb2868d06ef61294724d4faab296
devgiljong <calf-love@naver.com>
2024-05-11 19:07:04 +0900

    :construction: Add

--- jest.setup.ts
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom";
-import { server } from "./src/shared/mocks/node";
-import jestFetchMock from "jest-fetch-mock";
-jestFetchMock.enableMocks();
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
