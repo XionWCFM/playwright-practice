73a04306830e9f8fe5df101e03928f0cc6e4aeb1
devgiljong <calf-love@naver.com>
2024-05-11 18:47:59 +0900

    :construction: Test 추가

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+import { server } from "./src/shared/mocks/node";
+import jestFetchMock from "jest-fetch-mock";
+jestFetchMock.enableMocks();
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
